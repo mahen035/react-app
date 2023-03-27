e69da06bb7a31e21ac59fd3c39c5797e36e2a4ee
mahen <mahen518@gmail.com>
2023-03-27 21:23:42 +0530

    useReducer refactored

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,49 +1,55 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement',
+    INPUTCHANGE: 'inputChange',
+    TOGGLECOLOR: 'toggleColor'
+}
+
 const reducer = (state, action)=>{
     switch(action.type){
-        case 'increment':
+        case ACTIONS.INCREMENT:
             return {...state, counter:state.counter+1};
-        case 'decrement':
+        case ACTIONS.DECREMENT:
             return {...state, counter:state.counter-1};
-        case 'inputChange':
-            return {...state, inputVal:action.payload}     
+        case ACTIONS.INPUTCHANGE:
+            return {...state, inputVal:action.payload};
+        case ACTIONS.TOGGLECOLOR:
+            return {...state, color:!state.color}         
         case 'default':
             return state;
     }
 }
 
 function Reducer(props) {
-    //const[inputVal, setInputVal] = useState('');
-    //const[counter, setCounter] = useState(0)
-    const[state, dispatch] = useReducer(reducer, {counter:0, inputVal:''})
-    const[color, setColor] = useState(false)
-
+    const[state, dispatch] = useReducer(reducer, {counter:0, inputVal:'', color:false})
+    
     return (
-        <div className='form-group' style={{color:color?'red':'black'}}>
+        <div className='form-group' style={{color:state.color?'red':'black'}}>
 
             <input className='form-control'
               type="text"
               placeholder='Enter text'
-              onChange={(e)=>dispatch({type:'inputChange', payload:e.target.value})}
+              onChange={(e)=>dispatch({type:ACTIONS.INPUTCHANGE, payload:e.target.value})}
               /><br/><br/>
               <p>{state.inputVal}</p>
               <p>{state.counter}</p>
               <button 
                 type='button' 
                 className='btn btn-info'
-                onClick={()=>dispatch({type:'increment'})}>+</button> &nbsp;&nbsp;
+                onClick={()=>dispatch({type:ACTIONS.INCREMENT})}>+</button> &nbsp;&nbsp;
               <button 
                 type='button' 
                 className='btn btn-info'
-                onClick={()=>dispatch({type:'decrement'})}>-</button>
+                onClick={()=>dispatch({type:ACTIONS.DECREMENT})}>-</button>
                 <br/><br/>
                 <button 
                 type='button' 
                 className='btn btn-dark'
-                onClick={()=>setColor(!color)}>Toggle Color</button>  
+                onClick={()=>dispatch({type:ACTIONS.TOGGLECOLOR})}>Toggle Color</button>  
             
         </div>
     );
